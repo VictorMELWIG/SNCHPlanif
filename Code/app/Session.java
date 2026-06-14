112e1deb0c6f1894eee915179dfe3ddbcb501307
VictorMELWIG <victor.melwig@gmail.com>
2026-06-14 21:12:53 +0200

    Delete Code/app/Session.java

--- Code/app/Session.java
+++
@@ -1,9 +0,0 @@
-package snchbilletterie.app;
-
-import snchbilletterie.model.Trajet;
-import snchbilletterie.model.Utilisateur;
-
-public class Session {
-    public static Utilisateur user;
-    public static Trajet selectedTrajet;
-}
